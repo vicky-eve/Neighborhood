57b3f1a3e0514ac6783e79d2e54809d79d18abc1
vicky <victoria.awuor@student.moringaschool.com>
2022-04-15 15:08:35 +0300

    created the neighborhood class

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
+
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.TextField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hood",null=True)
+    health_contact = PhoneField(null=True, blank=True)
+    police_contact = PhoneField(null=True, blank=True)
+
