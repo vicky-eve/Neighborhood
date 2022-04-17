7672cf1fa3e6ae055840b522050e998462598fdc
vicky <victoria.awuor@student.moringaschool.com>
2022-04-17 14:30:11 +0300

    created a view for profile and and the index page

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -188,4 +188,9 @@ def index(request):
     return render (request, 'index.html')
 
 @login_required(login_url='/accounts/login')
-def profile(request)
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, 'registration/profile.html', {"profile":profile})
+
+ 
