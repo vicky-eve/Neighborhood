bd62d4aaafd0e9d5c3b582f6985c80baeeb84825
vicky <victoria.awuor@student.moringaschool.com>
2022-04-17 15:37:56 +0300

    created the forms

---
+++ neighborapp/forms.py
@@ -0,0 +1,24 @@
+from .models import Business, Neighborhood, Post, Profile
+from django.forms import ModelForm
+from django import forms
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('username','about', 'gen_location','nei_name') 
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields=['biz_name','email_address','contact']
+
+class NeighborhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ('admin',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields=['title','content','image']
